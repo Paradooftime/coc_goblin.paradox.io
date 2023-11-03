b6e3711efc72584431f7d3ff61966010ca68b846
paradoX <cbose344@gmail.com>
2023-11-03 12:36:23 +0530

    o7

--- coc.js
+++ coc.js
@@ -2,10 +2,17 @@ let menu = document.querySelector("#menu-icon");
 let navlist = document.querySelector(".navlist");
 var color = document.querySelector("#dadp");
 var colo = document.querySelector(".hero");
+var roloc = document.querySelector("#rtf");
+var colo = document.querySelector(".hero");
+
 
 color.onclick = () => {
-  document.body.classList.toggle("active");
+  document.body.classList.toggle("a2");
 };
+roloc.onclick = () => {
+  document.body.classList.toggle("a2");
+};
+
 
 menu.onclick = () => {
   menu.classList.toggle("bx-x");
@@ -33,4 +40,30 @@ sr.reveal(".icons", {
 sr.reveal(".scroll-down", {
   delay: 600,
   origin: "right",
+});
+const text = document.querySelector(".navlist");
+const p1 = document.querySelector("#dadp");
+const p2 = document.querySelector("#rtf");
+const ii1 = document.querySelector("#i1");
+const ii2 = document.querySelector("#i2");
+let num = 0;
+
+p1.addEventListener("click", () => {
+  if (num === 0) {
+    p1.style.display = "none";
+    p2.style.display = "block";
+    ii1.style.display = "none";
+    ii2.style.display = "block";
+    num = 1;
+  }
+});
+
+p2.addEventListener("click", () => {
+  if (num === 1) {
+    p2.style.display = "none";
+    p1.style.display = "block";
+    ii2.style.display = "none";
+    ii1.style.display = "block";
+    num = 0;
+  }
 });
